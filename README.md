5eaa1b7b02de450db83aa32667f841ec0f483431
blingbiu <41107184+blingbiu@users.noreply.github.com>
2021-09-30 15:40:23 +0800

    京东联盟SDK android集成；

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # react-native-mjdunion
+
 京东联盟SDK
+
+## Getting started
+
+`$ npm install react-native-mjdunion --save`
+
+### Mostly automatic installation
+
+`$ react-native link react-native-mjdunion`
+
+### Manual installation
+
+
+#### iOS
+
+1. In XCode, in the project navigator, right click `Libraries` ➜ `Add Files to [your project's name]`
+2. Go to `node_modules` ➜ `react-native-mjdunio` and add `RNReactNativeMjdunion.xcodeproj`
+3. In XCode, in the project navigator, select your project. Add `libRNReactNativeMjdunion.a` to your project's `Build Phases` ➜ `Link Binary With Libraries`
+4. Run your project (`Cmd+R`)<
+
+#### Android
+
+1. Open up `android/app/src/main/java/[...]/MainActivity.java`
+  - Add `import com.reactlibrary.RNReactNativeMjdunionPackage;` to the imports at the top of the file
+  - Add `new RNReactNativeMjdunionPackage()` to the list returned by the `getPackages()` method
+2. Append the following lines to `android/settings.gradle`:
+    	```
+    	include ':react-native-mjdunion'
+    	project(':react-native-mjdunion').projectDir = new File(rootProject.projectDir, 	'../node_modules/react-native-mjdunion/android')
+  ```
+3. Insert the following lines inside the dependencies block in `android/app/build.gradle`:
+    	```
+      compile project(':react-native-mjdunion')
+  ```
+
+## 其他配置
+
+#### ios
+将下载的京东联盟iOS SDK（包含 JDSDK.framework 和 JDSDK.bundle）引入到项目中
+URL Schemes 添加 sdkback + 京东联盟appKey
+LSApplicationQueriesSchemes 添加 openapp.jdpingou
+
+
+#### Android
+1. 替换模块中的安全图片`android/app/src/main/res/raw/safe.jpg`
+2. 打开 android/app/build.gradle ，在 defaultConfig 下添加:
+   ```
+   manifestPlaceholders = [
+   KeplerScheme  : "xxxxxx" //京东联盟SDK中AndroidManifest.xml 中的值
+   ]
+   ```
+3. 配置混淆`-keep class com.kepler.jd.**{ public <fields>; public <methods>; public *; }`
+
+## Usage
+```javascript
+import RNReactNativeMjdunion from 'react-native-mjdunion';
+
+// TODO: What to do with the module?
+RNReactNativeMjdunion;
+```
---
+++ android/src/main/java/com/jd/jdsdk/RNReactNativeMjdunionModule.java
@@ -0,0 +1,129 @@
+
+package com.jd.jdsdk;
+
+import android.app.Application;
+
+import com.facebook.react.bridge.*;
+
+import com.kepler.jd.Listener.AsyncInitListener;
+import com.kepler.jd.Listener.IOaidCallBck;
+import com.kepler.jd.Listener.OpenAppAction;
+import com.kepler.jd.login.KeplerApiManager;
+import com.kepler.jd.sdk.bean.KelperTask;
+import com.kepler.jd.sdk.bean.KeplerAttachParameter;
+
+public class RNReactNativeMjdunionModule extends ReactContextBaseJavaModule {
+
+    private final ReactApplicationContext reactContext;
+    private final KeplerAttachParameter mKeplerAttachParameter = new KeplerAttachParameter();// 这个是即时性参数
+
+    KelperTask mKelperTask;
+    int initKepler_success = 0;
+    OpenAppAction mOpenAppAction = new OpenAppAction() {
+        @Override
+        public void onStatus(int i, String s) {
+
+        }
+    };
+
+
+    public RNReactNativeMjdunionModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "RNReactNativeMjdunion";
+    }
+
+
+    @ReactMethod // 初始化SDK ----传入AppKey与AppSecret，获取config.xml下的配置
+    public void initSDK(final ReadableMap data, final Promise p) {
+        if (initKepler_success == 1) {
+            // 初始化成功
+            WritableMap map = Arguments.createMap();
+            map.putString("message", "success");
+            map.putString("code", Integer.toString(0));
+            p.resolve(map);
+            return;
+        }
+        String appKey = data.getString("appKey");
+        String appSecret = data.getString("appSecret");
+        String appAndroidID = data.getString("appAndroidID");
+        final String appOaid = data.getString("appOaid");
+
+        if (!appKey.equals("") && !appSecret.equals("")) {
+            Application app = (Application) reactContext.getApplicationContext();
+            KeplerApiManager.asyncInitSdk(app,
+                    appKey, appSecret, appAndroidID, new IOaidCallBck() {    // 用户的oaid，如果没有oaid可以返回其他唯一标示
+                        @Override
+                        public String getOaid() {
+                            return appOaid;
+                        }
+                    }, new AsyncInitListener() {
+                        public void onSuccess() {
+                            // 初始化成功
+                            initKepler_success = 1;
+                            WritableMap map = Arguments.createMap();
+                            map.putString("message", "success");
+                            map.putString("code", Integer.toString(0));
+                            p.resolve(map);
+                        }
+
+
+                        public void onFailure() {
+                            WritableMap map = Arguments.createMap();
+                            map.putString("message", "Kepler asyncInitSdk 授权失败，请检查lib 工程资源引用；包名,签名证书是否和注册一致");
+                            map.putString("code", Integer.toString(1));
+                            p.resolve(map);
+                        }
+                    });
+        } else {
+            WritableMap map = Arguments.createMap();
+            map.putString("message", "AppKey或AppSecret为空");
+            map.putString("code", Integer.toString(1));
+            p.resolve(map);
+        }
+    }
+
+    /**
+     * 通过sku唤起京喜APP或京东APP单品详情页
+     * ----传入itemID，isOpenByH5，processColor，backTagID，openType，customParams
+     */
+    @ReactMethod
+    public void showJXItemByUrl(final ReadableMap data, final Promise p) {
+        if (initKepler_success != 1) {
+            WritableMap map = Arguments.createMap();
+            map.putString("message", "未初始化SDK");
+            map.putString("code", Integer.toString(0));
+            p.resolve(map);
+            return;
+        }
+        String url = data.getString("url");
+
+        mKelperTask = KeplerApiManager.getWebViewService()
+                .openAppWebViewPageJX(reactContext, url, mKeplerAttachParameter,
+                        mOpenAppAction);
+
+    }
+
+    /**
+     * 通过url打开连接
+     * ----传入url，isOpenByH5，processColor，backTagID，openType，customParams
+     */
+    @ReactMethod
+    public void showItemByUrl(final ReadableMap data, final Promise p) {
+        if (initKepler_success != 1) {
+            WritableMap map = Arguments.createMap();
+            map.putString("message", "未初始化SDK");
+            map.putString("code", Integer.toString(0));
+            p.resolve(map);
+            return;
+        }
+        String url = data.getString("url");
+        mKelperTask = KeplerApiManager.getWebViewService()
+                .openAppWebViewPage(reactContext, url, mKeplerAttachParameter,
+                        mOpenAppAction);
+    }
+}
